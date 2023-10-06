88a9f80f990fc7aaaac2c8cd20dda8b09c6584ce
divinechukwuma <divinechukwuma982@gmail.com>
2023-10-06 04:00:01 -0700

    made changes to all my files

---
+++ 1.js
@@ -0,0 +1,18 @@
+let emailElement = document.querySelector('#email');
+let messageElement = document.querySelector('#message')
+console.log(emailElement);
+let typeElement = document.querySelector('.type')
+console.log(typeElement)
+let submitButton = document.querySelector('#sumbit-button')
+submitButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    let emailValue = emailElement.value
+    let messageValue = messageElement.value
+    console.log('email: ', emailValue)
+    console.log('message: ', messageValue)
+    if (emailValue.includes ('@')) {
+        alert('thank you for your message')
+    }
+    else {alert( 'that does not look like a valid email address, pls try again')}
+
+})
---
+++ 2.js
@@ -0,0 +1,9 @@
+console.log('hello world')
+const score = 30;
+const name = 'divine';
+const age = 30;
+ console.log (`my name is ${name} and i am ${age }`)
+
+
+ const s =('hello world');
+ console.log(s,lenght(11))
