9ef48430517629ed92b0c28e6473f89345443628
divinechukwuma <divinechukwuma982@gmail.com>
2023-10-07 08:36:39 -0700

    i added object oriented proggrams to my 2.js file

--- 2.js
+++ 2.js
@@ -17,74 +17,12 @@ const fruits = ['apples', 'goat', 'monkey', 'bannana',];
 fruits.push('strawberries');
 
 
-fruits.unshift('orange');
-
-
-fruits.pop();
+fruits.unshift('orange')
 
 console.log(fruits.indexOf('bannana'))
 
 
 console.log(fruits);
-
-const person = {
-    firstName: 'john',
-    lastName: 'divine',
-    age: 30,
-    hobbies: ['dancing', 'gaming', 'watching movies'],
-    address: {
-        street: 'foxtrox street',
-        city: 'abuja',
-        state: 'abia',
-    }
-}
-
-console.log(person.address.city)
-
-const { firstName, lastName, address: { city } } = person;
-console.log(city)
-
-person.email = 'divine@gmail.com'
-console.log(person)
-const todos = [
-    {
-        id: 1,
-        text: 'take  out the trash',
-        iscompleted: true
-    },
-    {
-        id: 2,
-        text: 'make money',
-        iscompleted: true
-    },
-    {
-        id: 3,
-        text: 'you are broke',
-        iscompleted: false
-    }
-];
-const todosJson = JSON.stringify(todos)
-console.log(todosJson)
-
-for (let i = 0; i <= 10; i++) {
-    console.log(`for loop number:${i}`);
-}
-let i = 0;
-while (i < 10) {
-    console.log(`while loop Number:${i}`);
-    i++
-}
-for (let todo of todos) {
-    console.log(todo.id)
-}
-
-const todoisCompleted = todos.filter(function (todo) {
-    return todo.iscompleted === true;
-}).map(function (todo) {
-    return todo.text;
-})
-console.log(todoisCompleted)
-
 
 const x = 20;
 const y = 40;
@@ -113,14 +51,22 @@ const  addNums= num1  =>
 num1 +5;
 
 console.log (addNums(5));
- 
-function Person  (firstName,lastName,dob){
-    this.firstName =firstName;
-    this.lastName =lastName;
-    this.dob;
-}
 
-const person1 = new person ( 'john','divine','4-3-1980')
-const person2 = new person ( 'mary','smith','3-6-1970')
+//constructor function
+ function Person(firstName,lastName,dob){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+    this.getBirthYear=function(){
+        return this.dob.getFullYear();
+    }
+ } 
+//instantiate object
 
-console.log(person2)
+ const person1 = new Person('john','doe','4-3-1980');
+
+ const person2 = new Person('oge','ejim','5-4-1180');
+
+ console .log(person2.dob.getFullYear());
+
+console .log (person1.getBirthYear());
