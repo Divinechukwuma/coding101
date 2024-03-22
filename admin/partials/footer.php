8555866127205cf3211311e5452bcc0663f9a6cc
divinechukwuma <divinechukwuma982@gmail.com>
2024-03-22 12:02:55 +0100

    last commit

--- admin/partials/footer.php
+++
@@ -1,8 +0,0 @@
-<div class="footer">
-        <div class="footer__wrapper">
-            <p class="text-center">2023 all rights reserved, food restaurant.Developed by <a href="#">Dev Divine</a></p>
-        </div>
-    </div>
-    <!-- footer section end -->
-</body>
-</html>
